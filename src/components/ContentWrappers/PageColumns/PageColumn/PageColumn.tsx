aa45c3ae7f1544970ae0e263815485573b2a4558
bdskrur <boyarskikh.ds@gmail.com>
2018-09-23 02:20:22 +0500

    pageColumns

--- src/components/ContentWrappers/PageColumns/PageColumn/PageColumn.tsx
+++ src/components/ContentWrappers/PageColumns/PageColumn/PageColumn.tsx
@@ -2,10 +2,15 @@ import * as React from "react";
 import cn from "./PageColumn.css";
 import Gapped from "retail-ui/components/Gapped/Gapped";
 
-export class PageColumn extends React.Component {
+interface IProps {
+    width?: number;
+}
+
+export class PageColumn extends React.Component<IProps> {
     public render() {
+        const { width } = this.props;
         return (
-            <div className={cn("column")}>
+            <div className={cn("column")} style={{ width }}>
                 <Gapped vertical={true} gap={20}>
                     {this.props.children}
                 </Gapped>
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { PageWithWorkspace } from "../../components/ContentWrappers/PageWithWorkspace/PageWithWorkspace";
 import { RateForm } from "../../components/Snatch/RateForm/RateForm";
 import { inject, observer } from "mobx-react";
 import { RootStore } from "../../stores";
@@ -7,6 +6,7 @@ import { NumberTitle } from "../../components/NumberTitle/NumberTitle";
 import { Gapped } from "retail-ui/components";
 import { SvgPieChartPie } from "../../components/SvgPieChart/SvgPieChartPie";
 import { Capitalization } from "../../components/Snatch/Capitalization/Capitalization";
+import { PageColumns } from "../../components/ContentWrappers/PageColumns/PageColumns";
 
 interface IProps {
     rootStore?: RootStore;
@@ -25,37 +25,35 @@ export class MainPage extends React.Component<IProps> {
 
     public render() {
         return (
-            <PageWithWorkspace>
-                <PageWithWorkspace.Body>
-                    <PageWithWorkspace.Column>
-                        <Gapped vertical={true} gap={15}>
-                            <NumberTitle
-                                value={this.snatchGeneratorStore.peoples.length}
-                                title="Игроков онлайн"
-                                style={{ background: "rgb(31, 45, 60)" }}
-                            />
-                            <NumberTitle
-                                value={this.snatchGeneratorStore.players.length}
-                                title="Игроков сделали вклад"
-                                style={{ background: "rgba(2, 166, 242, 0.5)" }}
-                            />
-                            <SvgPieChartPie
-                                data={this.snatchGeneratorStore.playersVsPeoplesPie}
-                                radius={150}
-                                hole={0}
-                                labels={true}
-                                percent={true}
-                            />
-                        </Gapped>
-                    </PageWithWorkspace.Column>
-                    <PageWithWorkspace.Column>
-                        <Capitalization />
-                    </PageWithWorkspace.Column>
-                </PageWithWorkspace.Body>
-                <PageWithWorkspace.Bar>
+            <PageColumns>
+                <PageColumns.Column>
+                    <Gapped vertical={true} gap={15}>
+                        <NumberTitle
+                            value={this.snatchGeneratorStore.peoples.length}
+                            title="Игроков онлайн"
+                            style={{ background: "rgb(31, 45, 60)" }}
+                        />
+                        <NumberTitle
+                            value={this.snatchGeneratorStore.players.length}
+                            title="Игроков сделали вклад"
+                            style={{ background: "rgba(2, 166, 242, 0.5)" }}
+                        />
+                        <SvgPieChartPie
+                            data={this.snatchGeneratorStore.playersVsPeoplesPie}
+                            radius={120}
+                            hole={0}
+                            labels={true}
+                            percent={true}
+                        />
+                    </Gapped>
+                </PageColumns.Column>
+                <PageColumns.Column>
+                    <Capitalization />
+                </PageColumns.Column>
+                <PageColumns.Column width={300}>
                     <RateForm />
-                </PageWithWorkspace.Bar>
-            </PageWithWorkspace>
+                </PageColumns.Column>
+            </PageColumns>
         );
     }
 
