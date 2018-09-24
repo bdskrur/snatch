506363df2bd3b6108d2be676478f0ec2bfa2816a
bdskrur <boyarskikh.ds@gmail.com>
2018-09-24 23:40:18 +0500

    animation change Number

--- src/components/NumberTitle/NumberTitle.tsx
+++ src/components/NumberTitle/NumberTitle.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import cn from "./NumberTitle.css";
 import { CSSProperties } from "react";
+import CountUp from "react-countup";
 
 interface IProps {
     value: number | string;
@@ -9,14 +10,22 @@ interface IProps {
 }
 
 export class NumberTitle extends React.Component<IProps> {
+    public componentWillUpdate(nextProps: IProps) {
+        this.prevValue = this.props.value;
+    }
+
     public render() {
         const { value, title, style } = this.props;
 
         return (
             <div className={cn("wrap")} style={style}>
-                <div className={cn("value")}>{value}</div>
+                <div className={cn("value")}>
+                    {typeof value === "number" ? <CountUp start={this.prevValue} end={value} /> : value}
+                </div>
                 {title ? <div className={cn("title")}>{title}</div> : null}
             </div>
         );
     }
+
+    private prevValue: number | string = 0;
 }
